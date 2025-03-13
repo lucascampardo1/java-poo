c9b250992ea96f9108c1de4bfe25799429cf544b
Lucas Campardo <lucascampardo1@gmail.com>
2025-03-13 19:55:22 -0300

    Aula 13/03/2025

--- src/model/Carro.java
+++ src/model/Carro.java
@@ -1,5 +1,44 @@
 package model;
 
 public class Carro {
+
+    private String modelo;
+    private String placa;
+    private String marca;
+    private int anoFabricacao;
+    private String cor;
     
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    
+
 }
